beb26c42dd4655136f49bdd2476170278a56ecfd
Renan Bet Rodrigues <renan@portaltelemedicina.com.br>
2019-12-11 18:46:28 -0300

    create authenticated routes

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+  <Route
+    {...rest}
+    render={props =>
+    isAuthenticated() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{pathname: "/", state: {from: props.location} }} />
+    )
+  } />
+)
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={() => <h1>Hello world</h1>} />
+      <PrivateRoute path="/app" component={() => <h1>Logado</h1>} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
